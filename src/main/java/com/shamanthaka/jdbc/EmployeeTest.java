01c5bd8fcf5d715c36583ca9ba5399bb0e13efc2
shamanthaka <shamanthaka.veerareddy@gmail.com>
2018-05-30 10:57:02 +0530

    added jdbc  update

--- src/main/java/com/shamanthaka/jdbc/EmployeeTest.java
+++ src/main/java/com/shamanthaka/jdbc/EmployeeTest.java
@@ -75,14 +75,66 @@ public class EmployeeTest {
         }
     }
 
+    public static void deleteEmployee(int id){
+        try{
+
+            Connection conn = getConnection();
+            //String query = "delete from Employee where id = ?";
+            String query = "delete from Employee where id = " + id;
+            Statement stmt = conn.createStatement();
+            boolean delete = stmt.execute(query);
+            if(delete){
+                System.out.println("Record is deleted it's id is " + id);
+            }
+
+        }catch (SQLException ex){
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+    }
+
+    public static void updateEmployee(String email, double salary, String empNo, int id){
+        try{
+
+            Connection con = getConnection();
+            Statement stmt = con.createStatement();
+            String query = "update employee set email = ?,salary = ?, emp_no =? where id = ?";
+
+            PreparedStatement preparedStatement = con.prepareStatement(query);
+
+            preparedStatement.setString(1,email);
+            preparedStatement.setDouble(2, salary);
+            preparedStatement.setString(3, empNo);
+            preparedStatement.setInt(4, id);
+
+            boolean update = preparedStatement.execute();
+            if(update){
+                System.out.println("Record is update...");
+            }
+
+        }catch (SQLException ex){
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+    }
+
 
     public static void main(String[] args){
         //EmployeeTest.addEmployee("Shamanthaka","Veerareddy","vinny@vinny.com",1234999.55,"100");
+        //EmployeeTest.deleteEmployee(2);
+        EmployeeTest.showEmployee();
+        EmployeeTest.updateEmployee("shamanthaka.veerareddy@gmail.com",9898988.8d, "121",1);
         EmployeeTest.showEmployee();
 
         System.out.println("Employee record is inserted...");
     }
+/*
 
+INSERT INTO employee(FIRST_NAME, LAST_NAME, EMAIL,SALARY, EMP_NO)
+					VALUES('Anitha','Veerareddy','anitha.veerareddy@gmail.com',23232.23, '123')
+ */
 
 
 }
