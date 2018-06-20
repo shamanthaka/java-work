293928ec88ce9cdea52653de001174b38dbbdeb7
shamanthaka <shamanthaka.veerareddy@gmail.com>
2018-06-20 09:51:15 +0530

    added jdbc  update

---
+++ src/main/java/com/shamanthaka/work/hranker/HR1.java
@@ -0,0 +1,55 @@
+package com.shamanthaka.work.hranker;
+
+/*
+Given an integer, , perform the following conditional actions:
+
+If n is odd, print Weird
+If n is even and in the inclusive range of 2 to 5, print Not Weird
+If n is even and in the inclusive range of 6 to 20 , print Weird
+If n is even and greater than 20, print Not Weird
+ */
+
+import java.util.Scanner;
+
+public class HR1 {
+
+    public void printWNW(){
+        System.out.println("Enter a number :");
+
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+
+        //for(int n = 1; n <= num; n++){
+            if(n >= 2 && n <= 5){
+                if(n % 2 == 0) {
+                    System.out.println("Not Weird");
+                }else{
+                    System.out.println("Weird");
+                }
+            }else if (n >= 6 && n <= 20){
+                if((n % 2 == 0)) {
+                    System.out.println("Weird");
+                }else{
+                    System.out.println("Not Weird");
+                }
+            }else if(n > 20){
+                if((n % 2 == 0)) {
+                    System.out.println("Not Weird");
+                }else{
+                    System.out.println("Wierd.");
+                }
+            }else{
+                if(n % 2 != 0){
+                    System.out.println("Wierd ..");
+                }
+            }
+        //}
+
+    }
+
+    public static void main(String[] args) {
+        HR1 hr = new HR1();
+        hr.printWNW();
+    }
+}
